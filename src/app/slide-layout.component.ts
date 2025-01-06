2af10d28f9193a3c9e073c22941031d34c078084
Julien CASTRO <julien.castro@rolex.com>
2025-01-06 11:12:34 +0100

    ajout du loader

--- src/app/slide-layout.component.ts
+++ src/app/slide-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet, RouterModule } from '@angular/router';
+import { Router, RouterOutlet, RouterModule, NavigationStart, NavigationEnd, Event } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-slide-layout',
@@ -8,15 +9,27 @@ import { CommonModule } from '@angular/common';
   imports: [RouterOutlet, RouterModule, CommonModule],
   template: `
     <div class="slide-container">
-      <div class="slide-content">
-
+      <div class="slide-content" *ngIf="!isLoading">
         <router-outlet></router-outlet>
       </div>
-      
+
       <nav class="nav-buttons">
         <button (click)="previousSlide()">Précédent</button>
         <button (click)="nextSlide()">Suivant</button>
       </nav>
+
+      <div *ngIf="isLoading" class="terminal-loader">
+        <div class="terminal-header">
+          <div class="terminal-title">Status</div>
+          <div class="terminal-controls">
+            <div class="control close"></div>
+            <div class="control minimize"></div>
+            <div class="control maximize"></div>
+          </div>
+        </div>
+        <div class="text">Loading...</div>
+      </div>
+
     </div>
   `,
   styles: [`
@@ -29,38 +42,131 @@ import { CommonModule } from '@angular/common';
       flex: 1;
     }
     .nav-buttons {
-      position: fixed; /* Change la position pour qu'elle soit fixe */
-      bottom: 2rem; /* Espace depuis le bas de la page */
-      left: 50%; /* Centre horizontalement */
-      transform: translateX(-50%); /* Ajuste pour centrer */
+      position: fixed;
+      bottom: 2rem;
+      left: 50%;
+      transform: translateX(-50%);
       display: flex;
       gap: 1rem;
     }
-    .background-iframe {
-      background:transparent;
-      width: 100%; /* Prend toute la largeur */
-      height: 100%; /* Prend toute la hauteur */
-      z-index: 0; /* Met l'iframe derrière le contenu */
+    @keyframes blinkCursor {
+      50% {
+        border-right-color: transparent;
+      }
     }
 
+    @keyframes typeAndDelete {
+      0%,
+      10% {
+        width: 0;
+      }
+      45%,
+      55% {
+        width: 6.2em;
+      } /* adjust width based on content */
+      90%,
+      100% {
+        width: 0;
+      }
+    }
+
+    .terminal-loader {
+      border: 0.1em solid #333;
+      background-color: #1a1a1a;
+      color: #0f0;
+      font-family: "Courier New", Courier, monospace;
+      font-size: 2.5em;
+      padding: 2.5em 1.5em;
+      width: 24em;
+      margin: 250px auto;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+      border-radius: 4px;
+      position: relative;
+      overflow: hidden;
+      box-sizing: border-box;
+    }
+
+    .terminal-header {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      height: 1.5em;
+      background-color: #333;
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+      padding: 0 0.4em;
+      box-sizing: border-box;
+    }
+
+    .terminal-controls {
+      float: right;
+    }
+
+    .control {
+      display: inline-block;
+      width: 0.6em;
+      height: 0.6em;
+      margin-left: 0.4em;
+      border-radius: 50%;
+      background-color: #777;
+    }
+
+    .control.close {
+      background-color: #e33;
+    }
+
+    .control.minimize {
+      background-color: #ee0;
+    }
+
+    .control.maximize {
+      background-color: #0b0;
+    }
+
+    .terminal-title {
+      float: left;
+      line-height: 1.5em;
+      color: #eee;
+    }
+
+    .text {
+      display: inline-block;
+      white-space: nowrap;
+      overflow: hidden;
+      border-right: 0.2em solid green; /* Cursor */
+      animation: typeAndDelete 4s steps(11) infinite,
+      blinkCursor 0.5s step-end infinite alternate;
+      margin-top: 1.5em;
+    }
   `]
 })
 export class SlideLayoutComponent {
+  isLoading = false;
   slides = [
-    '',  
-    'tech-stack',        
+    '',
+    'tech-stack',
     'app-mission',
     'about-rolex',
     'app-expectations',
     'app-mes-travaux',
     'app-conclusion'
-  
   ];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        this.isLoading = true;
+      } else if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 4000); // petti délai pour augmenter le temps d'affichage du loader
+      }
+    });
+  }
 
   getCurrentSlideIndex(): number {
-    const currentPath = this.router.url.slice(1); 
+    const currentPath = this.router.url.slice(1);
     return this.slides.indexOf(currentPath);
   }
 
