7dab6b1b318af1a0d8ef75da9554d2f65e61480f
Julien CASTRO <julien.castro@rolex.com>
2025-01-14 09:57:21 +0100

    ajout travaux 5 et 6

--- src/app/mes-travaux/mes-travaux.component.ts
+++ src/app/mes-travaux/mes-travaux.component.ts
@@ -14,8 +14,8 @@ export class MesTravauxComponent {
     { src: 'image (2).png', alt: 'Travail 2' },
     { src: 'image (3).png', alt: 'Travail 3' },
     { src: 'image (4).png', alt: 'Travail 4' },
-    { src: 'https://media.rolex.com/image/upload/q_auto:eco/f_auto/c_limit,w_1920/v1727879999/rolexcom/collection/family-pages/professional-watches/cosmograph-daytona/landing/cosmograph-daytona-landing-professional-watches-cosmograph-daytona-everose-gold_m126515ln-0006_2405jva_0003', alt: 'Travail 5' },
-    { src: 'https://media.rolex.com/image/upload/q_auto:eco/f_auto/c_limit,w_1920/v1711631705/rolexcom/collection/family-pages/professional-watches/cosmograph-daytona/landing/professional-watches-cosmograph-daytona-a-legendary-design', alt: 'Travail 6' },
+    { src: 'image (5).png', alt: 'Travail 5' },
+    { src: 'image (6).png', alt: 'Travail 6' },
     { src: 'https://media.rolex.com/image/upload/q_auto:eco/f_auto/c_limit,w_1920/v1708388531/rolexcom/about-rolex/rolex-history/1905-1919/rolex-history-1905-1919-hans-wilsdorf', alt: 'Travail 7' }
   ];
 
